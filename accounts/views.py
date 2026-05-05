de8c2500be91b1e325778f64810a812318d5474d
ahmedellaban1 <ellaban285@gmail.com>
2026-05-05 23:27:53 +0300

    add login and logout view && urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib import messages
 
-# Create your views here.
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard:home') # Redirect if already logged in
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, f"مرحباً بك مجدداً يا {user.first_name}!")
+            return redirect('dashboard:home')
+        else:
+            messages.error(request, "اسم المستخدم أو كلمة المرور غير صحيحة.")
+    
+    context = {
+        'page_title': 'تسجيل الدخول | عيادات الزهراء'
+    }
+    return render(request, 'accounts/login.html', context)
+
+def user_logout(request):
+    auth_logout(request)
+    context = {
+        'page_title': 'تم تسجيل الخروج | عيادات الزهراء'
+    }
+    return render(request, 'accounts/logout.html', context)
